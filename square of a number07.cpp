62d6aeb64968bb359b99db1de33efd7dc9634993
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-15 20:23:52 +0530

    Add files via upload

---
+++ square of a number07.cpp
@@ -0,0 +1,12 @@
+//7. Write a C++ program to calculate the square of a number
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	int a,squar;
+ 	cout<<"Enter a number";
+ 	cin>>a;
+ 	squar=a*a;
+ 	cout<<"square of a number is"<<squar;
+ 	
+ }
