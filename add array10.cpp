ff651892426a0a7ab75209cbc17004b5933410e3
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-15 20:24:54 +0530

    Add files via upload

---
+++ add array10.cpp
@@ -0,0 +1,15 @@
+//10. Write a C++ program to add all the numbers of an array of size 10.
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	int a[10],i,sum=0;
+ 	cout<<"Enter a 10 number";
+ 	for(i=0;i<10;i++)
+ 	{
+ 	cin>>a[i];
+    }
+    for(i=0;i<10;i++)
+    sum=sum+a[i];
+    cout<<"Addition of array is"<<sum;
+ }
