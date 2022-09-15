b9e107d2df8851815fa28544c3dd8f886d755d38
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-15 20:22:47 +0530

    Add files via upload

---
+++ area of circle05.cpp
@@ -0,0 +1,13 @@
+//4. Write a C++ program to calculate the area of a circle
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	int r;
+ 	float pi=3.14,a;//area=a;
+ 	cout<<"Enter a redius";
+ 	cin>>r;
+ 	a=pi*r*r;
+ 	cout<<"Area of circle is"<<a;
+
+ }
