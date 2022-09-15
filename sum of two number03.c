999e95630483ff1344fa37dc8f358025d6343537
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-15 20:20:10 +0530

    Add files via upload

---
+++ sum of two number03.c
@@ -0,0 +1,11 @@
+//Write a C++ program to calculate the sum of two numbers.
+#include<iostream>
+ using namespace std;
+ {
+ 	int a,b,c;
+ 	c=a+b;
+ 	cout<<"Enter  a and b";
+ 	cin>>a,b;
+ 	cout<<"sum of "<<a<<b<<c;
+ 	return 0;
+ }
