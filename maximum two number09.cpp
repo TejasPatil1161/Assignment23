101735608f951143737c66709bda02f6208e5832
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-15 20:24:27 +0530

    Add files via upload

---
+++ maximum two number09.cpp
@@ -0,0 +1,15 @@
+//9. Write a C++ program to find the maximum of two numbers.
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	int a,b;
+ 	cout<<"Enter a and b number";
+ 	cin>>a>>b;
+ 	if(a>b)
+	 {
+	 	cout<<"a is maximum";
+	 }
+	 else
+	 cout<<"b is maximum";
+ }
