3dd82fa79e73a9aea9ff1a068571e45cfe68af9e
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-15 20:19:22 +0530

    Add files via upload

---
+++ average of 3 numbers06.cpp
@@ -0,0 +1,12 @@
+//6. Write a C++ program to calculate an average of 3 numbers.
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	int a,b,c,avg 	;
+ 	cout<<"Enter three number";
+ 	cin>>a>>b>>c;
+ 	avg=a+b+c/100;
+ 	cout<<"Average of 3 numbers is"<<avg;
+ 	
+ }
